2c14d6d2d072a0dde12021f838f362d005c7f47a
Steffan Byrne <steffanbyrne@outlook.com>
2016-11-09 23:42:49 -0500

    Applied bugfixes from Amadren.

--- lgogd_uri/main.py
+++ lgogd_uri/main.py
@@ -42,7 +42,7 @@ GOGD_URI_CFG_PATH = "~/.config/lgogd_uri"
 PLAT_WIN = 1
 PLAT_MAC = 2
 PLAT_LIN = 4
-PLAT_DEF = 5  # Default
+PLAT_DEF = 4  # Default
 PLAT_ALL = 7
 SUBDIR_GAME = '%gamename%'
 SUBDIR_EXTRAS = 'extras'
@@ -338,20 +338,12 @@ class Application(dbus.service.Object):  # pylint: disable=C0111,R0902
         cmd = ['lgogdownloader']
         if is_inst:
             self.term.feed("\rRetrieving your shelf. This may take time.\r\n")
-            if is_patch:
-                cmd.extend(['--platform',
-                            str((win * PLAT_WIN) +
-                               (lin * PLAT_LIN) +
-                               (mac * PLAT_MAC)),
-                            '--download', '--no-extras', '--no-installers',
-                            '--game', '^%s$' % game_id])
-            else:
-                cmd.extend(['--platform',
-                            str((win * PLAT_WIN) +
-                               (lin * PLAT_LIN) +
-                               (mac * PLAT_MAC)),
-                            '--download', '--no-extras',
-                            '--game', '^%s$' % game_id])
+            cmd.extend(['--platform',
+                        str((win * PLAT_WIN) +
+                           (lin * PLAT_LIN) +
+                           (mac * PLAT_MAC)),
+                        '--download',
+                        '--game', '^%s$' % game_id, ' --exclude extras'])
         else:
             do_fix = self.builder.get_object("chk_path_fixup").get_active()
             if do_fix and not no_subdirs:
@@ -363,7 +355,7 @@ class Application(dbus.service.Object):  # pylint: disable=C0111,R0902
                     os.makedirs(tgt)
 
             path = "%s/%s" % (game_id, file_id)
-            cmd.extend(['--download-file', path])
+            cmd.extend(['--download-file', path, ' --exclude extras'])
 
         self.term.fork_command(cmd[0], cmd, None, tgt)
 
